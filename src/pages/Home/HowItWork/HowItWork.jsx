fe48835536542c3361954477f8de89e7deabe0e6
Alif-Hossen <naimuralif1.0@gmail.com>
2026-01-21 11:35:56 -0600

    How It Work Section Design Complete

--- src/pages/Home/HowItWork/HowItWork.jsx
+++ src/pages/Home/HowItWork/HowItWork.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
 import { LiaTruckPickupSolid } from 'react-icons/lia';
+import delivery1 from "../../../assets/HowWorks/delivery.avif"
 
 const HowItWork = () => {
     return (
-        <div className='bg-blue-300 max-w-6xl mx-auto my-16 px-2'>
-            <h1 className='py-4 text-4xl font-bold'>How It Works</h1>
+        <div className=' max-w-6xl mx-auto my-16 px-2'>
+            <h1 className='py-4 text-2xl lg:text-4xl font-bold'>How It Works</h1>
 
             <div className='grid lg:grid-cols-4 md:grid-cols-2 grid-1 lg:gap-6 md:gap-4 gap-2'>
                 <div>
-                    <div className='border rounded-xl p-2 my-2'>
+                    <div className=' bg-white rounded-xl p-2 my-2'>
                         <div>
-                            <LiaTruckPickupSolid />
+                            <img className='h-16 rounded-2xl' src={delivery1} alt="" />
                         </div>
                         <div>
                             <h1 className='text-xl font-bold py-2'>
@@ -23,44 +24,51 @@ const HowItWork = () => {
                     </div>
                 </div>
                 <div>
-                    <div className='border rounded-xl'>
+                    <div className=' bg-white rounded-xl p-2 my-2'>
                         <div>
-                            <LiaTruckPickupSolid />
+                            <img className='h-16 rounded-2xl' src={delivery1} alt="" />
                         </div>
                         <div>
-                            <h1>Booking Pick & Drop</h1>
-                            <p>
+                            <h1 className='text-xl font-bold py-2'>
+                                Booking Pick & Drop
+                            </h1>
+                            <p className='text-lg'>
                                 From personal packages to business shipments — we deliver on time, every time.
                             </p>
                         </div>
                     </div>
                 </div>
                 <div>
-                    <div className='border rounded-xl'>
+                    <div className=' bg-white rounded-xl p-2 my-2'>
                         <div>
-                            <LiaTruckPickupSolid />
+                            <img className='h-16 rounded-2xl' src={delivery1} alt="" />
                         </div>
                         <div>
-                            <h1>Booking Pick & Drop</h1>
-                            <p>
+                            <h1 className='text-xl font-bold py-2'>
+                                Booking Pick & Drop
+                            </h1>
+                            <p className='text-lg'>
                                 From personal packages to business shipments — we deliver on time, every time.
                             </p>
                         </div>
                     </div>
                 </div>
                 <div>
-                    <div className='border rounded-xl'>
+                    <div className=' bg-white rounded-xl p-2 my-2'>
                         <div>
-                            <LiaTruckPickupSolid />
+                            <img className='h-16 rounded-2xl' src={delivery1} alt="" />
                         </div>
                         <div>
-                            <h1>Booking Pick & Drop</h1>
-                            <p>
+                            <h1 className='text-xl font-bold py-2'>
+                                Booking Pick & Drop
+                            </h1>
+                            <p className='text-lg'>
                                 From personal packages to business shipments — we deliver on time, every time.
                             </p>
                         </div>
                     </div>
                 </div>
+                
             </div>
 
 
