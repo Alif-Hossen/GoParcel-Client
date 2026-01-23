cd29d7f87a05d5df423ec96afa7e88b2a877b8dc
Alif-Hossen <naimuralif1.0@gmail.com>
2026-01-23 03:30:07 -0600

    How It Section Design Complete Again

--- src/pages/Home/HowItWork/HowItWork.jsx
+++ src/pages/Home/HowItWork/HowItWork.jsx
@@ -1,24 +1,28 @@
 import React from 'react';
 import { LiaTruckPickupSolid } from 'react-icons/lia';
-import delivery1 from "../../../assets/HowWorks/delivery.avif"
+
+import howWork1 from "../../../assets/HowWorks/order1.avif"
+import howWork2 from "../../../assets/HowWorks/order2.avif"
+import howWork3 from "../../../assets/HowWorks/order3.avif"
+import howWork4 from "../../../assets/HowWorks/order4.avif"
 
 const HowItWork = () => {
     return (
         <div className=' max-w-6xl mx-auto my-16 px-2'>
             <h1 className='py-4 text-2xl lg:text-4xl font-bold'>How It Works</h1>
 
-            <div className='grid lg:grid-cols-4 md:grid-cols-2 grid-1 lg:gap-6 md:gap-4 gap-2'>
+            <div className='grid lg:grid-cols-4 md:grid-cols-2 grid-1 lg:gap-6 md:gap-4 gap-2  '>
                 <div>
                     <div className=' bg-white rounded-xl p-2 my-2'>
                         <div>
-                            <img className='h-16 rounded-2xl' src={delivery1} alt="" />
+                            <img className='h-20 w-30 rounded-2xl   ' src={howWork1} alt="" />
                         </div>
                         <div>
                             <h1 className='text-xl font-bold py-2'>
-                                Booking Pick & Drop
+                                Place Your Order
                             </h1>
                             <p className='text-lg'>
-                                From personal packages to business shipments — we deliver on time, every time.
+                                Create a delivery request in seconds by entering pickup and drop-off details. Choose the parcel type, preferred delivery option.
                             </p>
                         </div>
                     </div>
@@ -26,14 +30,14 @@ const HowItWork = () => {
                 <div>
                     <div className=' bg-white rounded-xl p-2 my-2'>
                         <div>
-                            <img className='h-16 rounded-2xl' src={delivery1} alt="" />
+                            <img className='h-20 w-30 rounded-2xl ' src={howWork2} alt="" />
                         </div>
                         <div>
                             <h1 className='text-xl font-bold py-2'>
-                                Booking Pick & Drop
+                                We Pick It Up
                             </h1>
                             <p className='text-lg'>
-                                From personal packages to business shipments — we deliver on time, every time.
+                                Our verified delivery partners collect your parcel from your doorstep at the scheduled time, ensuring safe handling from the very beginning.
                             </p>
                         </div>
                     </div>
@@ -41,14 +45,14 @@ const HowItWork = () => {
                 <div>
                     <div className=' bg-white rounded-xl p-2 my-2'>
                         <div>
-                            <img className='h-16 rounded-2xl' src={delivery1} alt="" />
+                            <img className='h-20 w-30 rounded-2xl ' src={howWork3} alt="" />
                         </div>
                         <div>
                             <h1 className='text-xl font-bold py-2'>
-                                Booking Pick & Drop
+                                Track in Real Time
                             </h1>
                             <p className='text-lg'>
-                                From personal packages to business shipments — we deliver on time, every time.
+                                Monitor your parcel’s journey with live tracking and instant status updates through the app, SMS, or email—so you’re always informed.
                             </p>
                         </div>
                     </div>
@@ -56,14 +60,14 @@ const HowItWork = () => {
                 <div>
                     <div className=' bg-white rounded-xl p-2 my-2'>
                         <div>
-                            <img className='h-16 rounded-2xl' src={delivery1} alt="" />
+                            <img className='h-20 w-30 rounded-2xl ' src={howWork4} alt="" />
                         </div>
                         <div>
                             <h1 className='text-xl font-bold py-2'>
-                                Booking Pick & Drop
+                                Delivered Safely
                             </h1>
                             <p className='text-lg'>
-                                From personal packages to business shipments — we deliver on time, every time.
+                                Your parcel is delivered securely to the destination. Receive confirmation upon successful delivery and enjoy a smooth, hassle-free experience.
                             </p>
                         </div>
                     </div>
