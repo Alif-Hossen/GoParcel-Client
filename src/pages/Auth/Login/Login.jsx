3b2d05a42f777314bd55ff3bf63989d4ff59249c
Alif-Hossen <naimuralif1.0@gmail.com>
2026-02-25 04:17:32 -0600

    Login Functionality Design Complete

--- src/pages/Auth/Login/Login.jsx
+++ src/pages/Auth/Login/Login.jsx
@@ -1,9 +1,67 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import useAuth from '../../../hooks/useAuth';
+import { Link } from 'react';
+import { NavLink } from 'react-router';
 
 const Login = () => {
+
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
+    const { signInUser } = useAuth()
+
+    const handleLogin = (data) => {
+        console.log('Login Data', data);
+        signInUser( data.email, data.password )
+            .then( result => {
+                console.log( result.user );
+            })
+            .catch( error => {
+                console.log( error );
+            })
+    }
+
     return (
-        <div>
-            <h2> This Is Login Page </h2>
+        <div className="card bg-base-100 w-full mx-auto max-w-sm shrink-0 shadow-2xl">
+            
+            <h3 className="text-3xl text-center font-bold">
+                Welcome Back
+            </h3>
+            <p className="text-center">Please Login</p>
+
+            <form className="card-body" onSubmit={handleSubmit(handleLogin)}> 
+                <fieldset className="fieldset">
+
+                    {/* EMAIL FIELD */}
+                    <label className="label">Email</label>
+
+                    <input type="email" {...register('email', {required: true })}  
+                    className="input" placeholder="Email" />
+
+                    {
+                        errors.email ?. type === 'required' 
+                        && ( <p className='text-red-500'> Email Is Required </p>)
+                    }
+
+                    {/* PASSWORD FIELD  */}
+                    <label className="label">Password</label>
+                    <input type="password" {...register('password',  { required : true, minLength: 6 })} className="input" placeholder="Password" /> 
+
+                    {
+                        errors.password ?. type === 'required' && 
+                        ( <p className='text red-500'> Password Is Required To Login </p>)
+                    }
+
+                    {
+                        errors.password ?. type === 'minlength' && 
+                        ( <p> Password Should At Least 6 Character. </p>)
+                    }
+
+                    <div><a className="link link-hover">Forgot password?</a></div>
+                    <button className="btn btn-neutral mt-4">Login</button>
+                </fieldset>
+                <p> New To Go Parcel? <NavLink to="/register" className="text-blue-500 font-bold underline">register</NavLink> </p>
+            </form>
         </div>
     );
 };
--- src/pages/Auth/Register/Register.jsx
+++ src/pages/Auth/Register/Register.jsx
@@ -1,63 +1,85 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import useAuth from '../../../hooks/useAuth';
+import { NavLink } from 'react-router';
+import SocialLogin from '../SocialLogin/SocialLogin';
 
 const Register = () => {
 
-    const { register, handleSubmit, formState: { errors }  } = useForm();
+    const { register, handleSubmit, formState: { errors } } = useForm();
 
-    const handleRegistration = ( data ) => {
-        console.log( 'After Registration : ', data );
+    const { registerUser } = useAuth();
+
+    const handleRegistration = (data) => {
+        console.log('After Registration : ', data);
+        registerUser(data.email, data.password)
+            .then(result => {
+                console.log(result.user);
+            })
+            .catch(error => {
+                console.log(error);
+            })
     }
 
 
     return (
-        <div>
-            <form onSubmit={handleSubmit(handleRegistration)}>
+        <div className="card bg-base-100 w-full mx-auto max-w-sm shrink-0 shadow-2xl">
+
+            <h3 className="text-3xl text-center font-bold">
+                Welcome To Go Parcel
+            </h3>
+            <p className="text-center">Please Register</p>
+
+
+            <form className="card-body" onSubmit={handleSubmit(handleRegistration)}>
                 <fieldset className="fieldset">
 
                     <label className="label">Email</label>
                     <input type="email" {...register('email',
-                         {
+                        {
                             required: true,
-                         }
-                         )} className="input" placeholder="Email" />
+                        }
+                    )} className="input" placeholder="Email" />
 
-                         { errors.email ?. type==='required'
-                            &&  <p className='text-red-500'> Email Is Required. </p>
-                         }
+                    {errors.email?.type === 'required'
+                        && <p className='text-red-500'> Email Is Required. </p>
+                    }
 
                     {/* PASSWORD FIELD...... */}
                     <label className="label">Password</label>
-                    <input type="password" {...register('password', 
-                         {   
+                    <input type="password" {...register('password',
+                        {
                             required: true,
                             minLength: 6,
-                            pattern : /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[^A-Za-z0-9]).+$/
+                            pattern: /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[^A-Za-z0-9]).+$/
 
-                         }
+                        }
                     )} className="input" placeholder="Password" />
 
                     {
-                        errors.password ?. type === 'required'
+                        errors.password?.type === 'required'
                         && <p className='text-red-500'> Password Required To Register. </p>
                     }
 
                     {
-                        errors.password ?. type === 'minLength'
+                        errors.password?.type === 'minLength'
                         && <p className='text-red-500'> Password Must Be 6 Character Or Longer. </p>
                     }
                     {
-                        errors.password ?. type === 'pattern'
+                        errors.password?.type === 'pattern'
                         && <p className='text-red-500'> Password Must Have At Least One Uppercase, At Least One Lowercase, At Least One Number, And At Least One Special Characters</p>
                     }
 
-                    <div>
+                    {/* <div>
                         <a className="link link-hover">Forgot password?</a>
-                    </div>
+                    </div> */}
 
-                    <button className="btn btn-neutral mt-4">Login</button>
+                    <button className="btn btn-neutral mt-4">Register</button>
                 </fieldset>
+                
+                <p> Already Have An Account? <NavLink to="/login" className="text-blue-500 font-bold underline">Login</NavLink> </p>
             </form>
+            <SocialLogin></SocialLogin>
         </div>
     );
 };
