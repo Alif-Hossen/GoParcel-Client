8fcbcdaaa695daf100d8f2e23f43272380f4b9fe
Alif-Hossen <naimuralif1.0@gmail.com>
2026-04-05 12:38:34 -0600

    Display Riders Available To Pickup A Parcel

--- src/pages/Dashboard/AssignRiders/AssignRiders.jsx
+++ src/pages/Dashboard/AssignRiders/AssignRiders.jsx
@@ -1,11 +1,11 @@
 import { useQuery } from '@tanstack/react-query';
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import useAxiosSecure from '../../../hooks/useAxiosSecure';
 
 const AssignRiders = () => {
 
+    const [selectedParcel, setSelectedParcel] = useState(null);
     const axiosSecure = useAxiosSecure();
-
     const riderModalRef = useRef();
 
 
@@ -17,8 +17,33 @@ const AssignRiders = () => {
         }
     })
 
+    const { data: riders = [] } = useQuery({
+
+        // EKHANE OPTIONAL CHAINING NA DILE ERROR ASHTECHILO -->
+
+        queryKey: ['riders', selectedParcel?.senderDistrict, 'available'],
+        enabled: !!selectedParcel,
+        queryFn: async () => {
+            const res = await axiosSecure.get(`/riders?status=approved&district=${selectedParcel.senderDistrict}&workStatus=available`);
+            return res.data;
+        }
+    })
+
     const openAssignRiderModal = parcel => {
+        setSelectedParcel(parcel);
+        console.log(parcel.senderDistrict)
         riderModalRef.current.showModal()
+    }
+
+    const handleAssignRider = rider => {
+        const riderAssignInfo = {
+            riderId: rider._id,
+            riderEmail: rider.email,
+            riderName: rider.name,
+            parcelId: selectedParcel._id,
+        }
+        axiosSecure.patch(``, riderAssignInfo)
+
     }
 
     return (
@@ -32,8 +57,8 @@ const AssignRiders = () => {
                     {/* head */}
                     <thead>
                         <tr>
-                            <th></th>
-                            <th>Name</th>
+                            <th> No</th>
+                            <th>Parcel Name</th>
                             <th>Cost</th>
                             <th>Created At</th>
                             <th>Pickup District</th>
@@ -65,11 +90,40 @@ const AssignRiders = () => {
 
             <dialog ref={riderModalRef} className="modal modal-bottom sm:modal-middle">
                 <div className="modal-box">
-                    <h3 className="font-bold text-lg">Hello!</h3>
-                    <p className="py-4">Press ESC key or click the button below to close</p>
+                    <h3 className="font-bold text-lg">Riders: {riders.length}!</h3>
+
+                    <div className="overflow-x-auto">
+                        <table className="table table-zebra">
+                            {/* head */}
+                            <thead>
+                                <tr>
+                                    <th></th>
+                                    <th>Name</th>
+                                    <th>Job</th>
+                                    <th>Favorite Color</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    riders.map( (rider, i)=> <tr key={rider._id}>
+                                        <th>{i+1}</th>
+                                        <td>{rider.name}</td>
+                                        <td>{rider.email}</td>
+                                        <td>
+                                            <button 
+                                            onClick={() => handleAssignRider(rider)}
+                                            className='btn btn-primary text-black'>Assign</button>
+                                        </td>
+                                    </tr>)
+                                }
+
+
+                            </tbody>
+                        </table>
+                    </div>
+
                     <div className="modal-action">
                         <form method="dialog">
-                            {/* if there is a button in form, it will close the modal */}
                             <button className="btn">Close</button>
                         </form>
                     </div>
