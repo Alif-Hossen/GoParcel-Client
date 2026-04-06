4efae9fc5aeed5985550e90c5d38a482341e1547
Alif-Hossen <naimuralif1.0@gmail.com>
2026-04-06 11:02:28 -0600

    Accept Parcel And Explore

--- src/pages/Dashboard/AssignDeliveries/AssignDeliveries.jsx
+++ src/pages/Dashboard/AssignDeliveries/AssignDeliveries.jsx
@@ -2,20 +2,40 @@ import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import useAuth from '../../../hooks/useAuth';
 import useAxiosSecure from '../../../hooks/useAxiosSecure';
+import Swal from 'sweetalert2';
 
 const AssignDeliveries = () => {
 
     const { user } = useAuth();
     const axiosSecure = useAxiosSecure();
 
-    const { data: parcels = [] } = useQuery({
+    const { data: parcels = [], refetch } = useQuery({
         queryKey: ['parcels', user.email, 'driver_assigned'],
         queryFn: async () => {
-            const res = await axiosSecure.get(`/parcels/rider?riderEmail=${user.email}&deliverStatus=driver_assigned`)
+            const res = await axiosSecure.get(`/parcels/rider?riderEmail=${user.email}&deliveryStatus=driver_assigned`)
             return res.data;
         }
 
+
     })
+
+    const handleAcceptDelivery = parcel => {
+        const statusInfo = { deliveryStatus: 'rider_arriving' }
+        axiosSecure.patch(`/parcels/${parcel._id}/status`, statusInfo)
+            .then(res => {
+                if (res.data.modifiedCount) {
+                    refetch();
+                    Swal.fire({
+                        position: "top-end",
+                        icon: "success",
+                        title: `Thank You For Accepting.`,
+                        showConfirmButton: false,
+                        timer: 2500
+
+                    });
+                }
+            })
+    }
 
     return (
         <div>
@@ -31,22 +51,33 @@ const AssignDeliveries = () => {
                             <th>No</th>
                             <th>Name</th>
                             <th>Confirm</th>
-                            <th>Favorite Color</th>
+                            <th>Other Actions</th>
                         </tr>
                     </thead>
                     <tbody>
 
                         {
                             parcels.map((parcel, index) => <tr key={parcel._id}>
-                                <th>{index+1}</th>
+                                <th>{index + 1}</th>
                                 <td>{parcel.parcelName}</td>
-                                <td> 
-                                    <button className='btn btn-primary text-black'>
-                                        Accept
-                                    </button>
-                                    <button className='btn btn-warning text-black ms-2'>
-                                        Reject
-                                    </button>
+                                <td>
+                                    {
+                                        parcel.deliveryStatus === 'driver_assigned' ? <>
+                                            <button
+                                                onClick={() => handleAcceptDelivery(parcel)}
+                                                className='btn btn-primary text-black'>
+                                                Accept
+                                            </button>
+                                            <button className='btn btn-warning text-black ms-2'>
+                                                Reject
+                                            </button>
+                                        </>
+
+                                        : <span>
+                                            Accepted
+                                        </span>
+
+                                    }
                                 </td>
                                 <td>Blue</td>
                             </tr>)
